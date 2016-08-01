a4d0529d9dcb65c2b7d018501b358cfc73cfc817
Christopher Nyberg <christopher.nyberg@gmail.com>
2016-07-31 19:34:57 -0700

    Added some error checking
    Not every use case, but solid for now. I need to figure out how to turn
    all runtime checks on using stack ghc.

--- todoApp.hs
+++ todoApp.hs
@@ -5,9 +5,40 @@ import           System.Environment
 import           System.IO
 
 dispatch :: String -> [String] -> IO ()
-dispatch "add"    = add
-dispatch "view"   = view
-dispatch "remove" = remove
+dispatch "add"       = add
+dispatch "view"      = view
+dispatch "remove"    = remove
+dispatch "bump"      = bump
+dispatch wrongInput  = failMessage wrongInput
+disptach _           = fullBlownError
+
+
+bump :: [String] -> IO ()
+bump [fileName, numberString] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      number = read numberString
+      newTodoItems = unlines $ (todoTasks !! number) : delete (todoTasks !! number) todoTasks
+  bracketOnError (openTempFile "." "temp")
+    (\(tempName, tempHandle) -> do
+      hClose tempHandle
+      removeFile tempName)
+    (\(tempName, tempHandle) -> do
+      hPutStr tempHandle newTodoItems
+      hClose tempHandle
+      removeFile fileName
+      renameFile tempName fileName)
+
+
+
+fullBlownError :: IO ()
+fullBlownError =
+  putStrLn "Invalid input, please try again."
+
+
+failMessage :: String -> [String] -> IO ()
+failMessage msg _ =
+  putStrLn $ "I'm sorry I don't recognize command " ++ msg
 
 
 add :: [String] -> IO ()
@@ -22,15 +53,14 @@ view [fileName] = do
                               [0..] todoTasks
   putStr $ unlines numberedTasks
 
+
 remove :: [String] -> IO ()
 remove [fileName, numberString] = do
   contents <- readFile fileName
   let todoTasks = lines contents
       numberedTasks = zipWith (\n line -> show n ++ " - " ++ line )
                               [0..] todoTasks
-  putStrLn "These are your ToDo items: "
-  mapM_ putStrLn numberedTasks
-  let number = read numberString
+      number = read numberString
       newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
   bracketOnError (openTempFile "." "temp")
     (\(tempName, tempHandle) -> do
@@ -41,7 +71,9 @@ remove [fileName, numberString] = do
       hClose tempHandle
       removeFile fileName
       renameFile tempName fileName)
-
+  putStrLn "These are your remaining Todo items: "
+  mapM_ putStrLn $ zipWith (\n line -> show n ++ " - " ++ line)
+                           [0..] $ lines newTodoItems
 
 main = do
   (command:argList) <- getArgs
